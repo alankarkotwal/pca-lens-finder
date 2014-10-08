f7bb0bdcd2c3e3294620cef7244405b906526be8
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-10-08 15:06:16 +0530

    added filenames in main, edited gitignore

--- code/main.m
+++ code/main.m
@@ -1,7 +1,8 @@
 % Main file for pca-lens-finder.
 % Author: Alankar Kotwal
 
+addpath('code');
+
 % Getting the files
 % By default, the testing and training set is located in images/
-cd images/
-dataset = dir;
+filelist = build_files('images');
